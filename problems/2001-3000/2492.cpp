92f6e9e59576fa7e27fe11710f2263280af5e37e
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-07-24 10:39:59 +0900

    Solve 2492

---
+++ problems/2001-3000/2492.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n, max_w, v[1005], w[1005];
+int ans = -1;
+
+int main() {
+  cin >> n >> max_w;
+  for (int i = 1; i <= n; i++) {
+    cin >> v[i] >> w[i];
+
+    if (w[i] <= max_w) {
+      ans = max(ans, v[i]);
+    }
+  }
+  cout << ans << endl;
+
+  return 0;
+}
