d8c988a8d5f51ad2e877410d992c51f2ba5a046a
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-08-17 06:10:01 +0000

    Solve 2776

---
+++ problems/2001-3000/2776.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int a, b, h, w;
+
+int main() {
+  cin >> a >> b;
+  cin >> h >> w;
+
+  if (a == b || h == w) {
+    cout << "Same" << endl;
+    return 0;
+  }
+
+  if (a < b && h < w) {
+    cout << "Non-rotating" << endl;
+  } else if (a > b && h > w) {
+    cout << "Non-rotating" << endl;
+  } else {
+    cout << "Rotating" << endl;
+  }
+
+  return 0;
+}
