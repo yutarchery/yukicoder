1b052846a7c1b2489ed572baa12bc701726bb6dd
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-08-17 06:13:55 +0000

    Solve 2777

---
+++ problems/2001-3000/2777.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n, k, a[int(1e5 + 5)];
+int cnt[int(1e5 + 5)];
+
+int main() {
+  cin >> n >> k;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+    cnt[a[i]]++;
+  }
+
+  for (int i = 1; i <= n; i++) {
+    if (cnt[i] + cnt[0] >= k) {
+      Yes();
+      return 0;
+    }
+  }
+  No();
+
+  return 0;
+}
